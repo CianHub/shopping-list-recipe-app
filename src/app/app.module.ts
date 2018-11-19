b5fc300d80a3d81390c24d0f79ce8f2f2af1a208
Cian O'Gralaigh <ihatecian@gmail.com>
2018-11-19 21:17:18 +0000

    Set up Angular workspace, create Angular Material module, add Angular Material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular Material Animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Module
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
