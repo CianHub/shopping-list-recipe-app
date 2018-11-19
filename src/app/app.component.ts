19ab07e9fb140ec33de9297e7c1653f8537c10a1
Cian O'Gralaigh <ihatecian@gmail.com>
2018-11-19 22:35:51 +0000

    Add two mock components, add comments to appComponent files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  /* The tag that corresponds to this component in a HTML file, works like a CSS selector
+  Due to this you can do it in other ways like as an attribute or class
+  This can be handy for add a component when an element has a class */
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html', // The HTML code for this component
+  styleUrls: ['./app.component.scss'] // The styles for the HTML template for this component
 })
-export class AppComponent {
-  title = 'course-project';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 // Angular Material Animations
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,10 +10,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AngularMaterialModule],
+  // A module is used to bundle components together
+  // NgModule decorator marks the class as a module for the compiler
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
