147a9212e9dc05904d7b68e4eabd5d87a4d62dd2
Cian O'Gralaigh <ihatecian@gmail.com>
2018-12-26 22:31:02 +0000

    Add recipe-book service methods
    Injected the recipe-book service into the recipe-book component and it's children.
    Replaced the input/outpt functionality of selecting a recipe with a leaner service based way of doing it.
    This uses a service to assigned the selected recipe to an subscribable property in the service.
    This property can be accessed by any of the recipe components.

--- src/app/modules/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/modules/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeBookService } from 'src/app/services/recipe-book-service/recipe-book.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,28 +8,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
-
   // Property is an array of Recipe models
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test Recipe',
-      'This is just a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Pug_-_1_year_Old.jpg/1200px-Pug_-_1_year_Old.jpg'
-    ),
-    new Recipe(
-      'Another test Recipe',
-      'This is just a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Pug_-_1_year_Old.jpg/1200px-Pug_-_1_year_Old.jpg'
-    )
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipeService: RecipeBookService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   public selectRecipe(recipe: Recipe) {
-    console.log(recipe);
-    this.selectedRecipe.emit(recipe);
+    this.recipeService.selectedRecipe.emit(recipe);
   }
 }
--- src/app/services/recipe-book-service/recipe-book.service.ts
+++ src/app/services/recipe-book-service/recipe-book.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Recipe } from 'src/app/modules/recipe-book/recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeBookService {
+  // This property will emit the Recipe object assigned to it
+  selectedRecipe = new EventEmitter<Recipe>();
 
-  constructor() { }
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is just a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Pug_-_1_year_Old.jpg/1200px-Pug_-_1_year_Old.jpg'
+    ),
+    new Recipe(
+      'Another test Recipe',
+      'This is just a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Pug_-_1_year_Old.jpg/1200px-Pug_-_1_year_Old.jpg'
+    )
+  ];
+
+  // Create a copy of the recipes array
+  public getRecipes = () => this.recipes.slice();
+
+  constructor() {}
 }
