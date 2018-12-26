2ab54827c07aa6abca06a2ad9eac3b905f663fa4
Cian O'Gralaigh <ihatecian@gmail.com>
2018-12-26 23:12:09 +0000

    Replace input/output methods with service
    Simplied adding new ingredient functionality by replacing the overly complex existing methods with
    shoppping-list-service.
    The ingredient list is now dynamic and any component with access can show the current list.

--- src/app/modules/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/modules/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -3,10 +3,10 @@ import {
   OnInit,
   ViewChild,
   ElementRef,
-  Output,
   EventEmitter
 } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/services/shopping-list-service/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -14,19 +14,16 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./shopping-list-edit.component.scss']
 })
 export class ShoppingListEditComponent implements OnInit {
-  newIngredient: Ingredient;
-
-  @Output() ingredient = new EventEmitter<Ingredient>();
-
   @ViewChild('nameInput') nameInput: ElementRef;
   @ViewChild('amountInput') amountInput: ElementRef;
 
-  constructor() {}
+  constructor(private shoppingService: ShoppingListService) {}
 
   ngOnInit() {}
 
   public addIngredient = () =>
-    this.ingredient.emit(
+    // Make the new ingredient accessible to every component with access to the service
+    this.shoppingService.ingredient.emit(
       new Ingredient(
         this.nameInput.nativeElement.value,
         this.amountInput.nativeElement.value
