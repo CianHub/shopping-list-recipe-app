d04ac16223938d5e13ad53418d950371deb69f2d
Cian O'Gralaigh <ihatecian@gmail.com>
2019-01-08 22:42:02 +0000

    Add reactive forms to recipe-edit
    Added reactive froms skeleton functioinality to recipe edit page.
    Added basic validation.

--- src/app/modules/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/modules/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormBuilder, Validators, FormControl, FormArray, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormArray,
+  FormGroup,
+  Form
+} from '@angular/forms';
 import { RecipeBookService } from 'src/app/services/recipe-book-service/recipe-book.service';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from 'src/app/shared/ingredient.model';
@@ -13,70 +20,74 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
-  newRecipe: Recipe;
-  constructor(private route: ActivatedRoute, private fb: FormBuilder, private recipeService: RecipeBookService) {}
+  recipeForm: FormGroup;
 
-  recipeForm = this.fb.group({
-    name: [''],
-    description: [''],
-    image: [''],
-    ingredients: this.fb.array([])
-  }, Validators.required );
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeBookService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'];
+      this.initForm();
     });
   }
 
-  get ingredientsArray(): FormArray {
-    return this.recipeForm.get('ingredients') as FormArray;
+  public onSubmit() {
+    console.log(this.recipeForm.value);
   }
 
-  public addIngredientField = () => this.ingredientsArray.push(new FormGroup({
-    ingredientName: new FormControl(null),
-    ingredientAmount: new FormControl(null)
-  }))
+  addIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl([
+          null,
+          Validators.required,
+          Validators.pattern(/^[0-9+[0-9]*$/)
+        ])
+      })
+    );
+  }
 
-  public errorMessage = (formControlName) => {
-    if (this.recipeForm.get(formControlName).errors) {
-      return 'A value is required for this field';
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImage = '';
+    let recipeDesc = '';
+    const recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id)[0];
+      recipeName = recipe.name;
+      recipeImage = recipe.imagePath;
+      recipeDesc = recipe.description;
+
+      if (recipe['ingredients']) {
+        for (const ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[0-9+[0-9]*$/)
+              ])
+            })
+          );
+        }
+      }
+
+      this.recipeForm = new FormGroup({
+        name: new FormControl(recipeName, Validators.required),
+        imagePath: new FormControl(recipeImage, Validators.required),
+        description: new FormControl(recipeDesc, Validators.required),
+        ingredients: recipeIngredients
+      });
     }
   }
-  public resetRecipe = () => this.recipeForm.reset();
-
-  public addRecipe = () => {
-    this.recipeForm.valid ?
-    this.recipeService.recipes.push(this.recipeMaker(this.idMaker(), this.ingredientMaker())) : this.touchForm();
-  }
-
-  public idMaker = () => {
-    return this.recipeService.recipes.length + 1; }
-
-  public ingredientMaker = () => {
-    const newIngredients = [];
-    this.recipeForm.value.ingredients.map(values => {
-      const newIng = new Ingredient(values.ingredientName, values.ingredientAmount);
-      newIngredients.push(newIng);
-    });
-    return newIngredients;
-  }
-
-  public touchForm = () => {
-    this.recipeForm.get('name').markAsTouched();
-    this.recipeForm.get('description').markAsTouched();
-    this.recipeForm.get('image').markAsTouched();
-    this.recipeForm.get('ingredients').markAsTouched();
-  }
-
-  public recipeMaker = (id, newIngredients) => {
-    return new Recipe(
-      id,
-      this.recipeForm.value.name,
-      this.recipeForm.value.description,
-      this.recipeForm.value.image,
-      newIngredients);
-    }
-
 }
